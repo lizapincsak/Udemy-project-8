251b6cd32a49f907da5476181fc3717b6490a1b1
lizapincsak <liza.pincsak@gmail.com>
2021-09-12 10:12:33 -0500

    error boundary

--- src/components/Users.js
+++ src/components/Users.js
@@ -12,11 +12,20 @@ class Users extends Component {
       showUsers: true
     };
   }
+
+  componentDidUpdate(){
+    
+    if(this.props.users.length === 0){
+      throw new Error('No users provided!')
+    }
+  }
+
   toggleUsersHandler(){
     this.setState((curState) => {
       return {showUsers: !curState.showUsers}
     });
   }
+
   render () {
     const usersList = (
       <ul>
